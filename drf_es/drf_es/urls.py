dbe3120f48f18103b50723967fb8e33f26770659
wzyonggege <jamwong994@gmail.com>
2018-02-27 22:46:52 +0800

    add

--- drf_es/drf_es/urls.py
+++ drf_es/drf_es/urls.py
@@ -16,9 +16,12 @@ Including another URLconf
 from django.conf.urls import url, include
 from django.contrib import admin
 
+from django.views.generic import TemplateView
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'', include('search.urls'))
+    url(r'^$', TemplateView.as_view(template_name='index.html'), name='index'),
+    url(r'^api/', include('search.urls'))
 ]
 
 
--- drf_es/search/urls.py
+++ drf_es/search/urls.py
@@ -5,11 +5,5 @@ from . import api
 
 app_name = 'search'
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
-    url(r'^result$', views.result, name='result'),
-    url(r'^search/$', views.search, name='search'),
-    url(r'^suggest/$', views.suggest, name='suggest'),
-
-    # api
-    url(r'^api/list/$', api.StackoverflowView.as_view(), name='search_list'),
+    url(r'list/$', api.StackoverflowView.as_view(), name='search_list'),
 ]
